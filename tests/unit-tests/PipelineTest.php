80f194f694c6617aa3769cac7ba34a3ce98a2b91
Konstantin Stoldt <konstantin.stoldt@talk-point.de>
2016-02-07 17:30:15 +0100

    Pipeline: implement many failover

--- tests/unit-tests/PipelineTest.php
+++ tests/unit-tests/PipelineTest.php
@@ -68,4 +68,28 @@ class PipelineTest extends TestCase
         $this->assertEquals(true, $pipeline->is_runned);
         $this->assertEquals(true, $pipeline->is_failed);
     }
+
+    public function test_run_two_commands_failover()
+    {
+        // Mock Job
+        $job = Mockery::mock(\TheCodeEngine\Pipeline\Job::class);
+        // Mock Command
+        $command2 = Mockery::mock(\TheCodeEngine\Pipeline\Command::class);
+        $command2->shouldReceive('exec')->once()->andReturn([false, null, null]);
+        $command2->shouldReceive('undo_run')->once()->andReturn(true);
+        $command2->shouldReceive('isRunned')->once()->andReturn(false);
+        // Mock Command1
+        $command1 = Mockery::mock(\TheCodeEngine\Pipeline\Command::class);
+        $command1->shouldReceive('exec')->once()->andReturn([true, $command2, null]);
+        $command1->shouldReceive('undo_run')->once()->andReturn(true);
+        $command1->shouldReceive('isRunned')->between(1, 3)->andReturnValues([false, true]);
+
+        $pipeline = new Pipeline($job, $command1);
+        $this->assertEquals(false, $pipeline->is_runned);
+        $this->assertEquals(false, $pipeline->is_failed);
+        $pipeline->run();
+        $this->assertEquals(2, count($pipeline->commands));
+        $this->assertEquals(true, $pipeline->is_runned);
+        $this->assertEquals(true, $pipeline->is_failed);
+    }
 }
